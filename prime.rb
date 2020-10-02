872164b213a2155e19a6697aa8186a6308a75a42
Randi Schreiner <mimirandi21@gmail.com>
2020-10-02 09:09:45 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+#find prime number
+#add them to an array
+#divide all other numbers by this array to determine if prime
+require 'pry'
+def prime?(integer)
+    return true if integer == 2
+    return false if integer <=1 || integer.even?
+    
+
+    bunch_o_numbers = (2..integer).to_a
+    i =3
+    while i < integer
+        return false if integer % i == 0
+        i+=2
+
+    end
+    true
+end
